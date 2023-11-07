23e007432dc93dbde3040dab6e614091c8db6272
Daniele Giona <148854664+ilGiona@users.noreply.github.com>
2023-11-07 10:24:17 +0100

    Add files via upload

---
+++ Lab2023_10_24/DivisioniZ.java
@@ -0,0 +1,15 @@
+public class DivisioniZ
+{
+	public static void main(String[] args)
+	{
+		int m = 7;
+		int n = 2;
+		int risultato = m/n;
+		System.out.print("Valore M = ");
+		System.out.println(m);
+		System.out.print("Valore N = ");
+		System.out.println(n);
+		System.out.print("Risultato M/N = ");
+		System.out.println(risultato);
+	}
+}
---
+++ Lab2023_10_31/Media.java
@@ -0,0 +1,12 @@
+public class Media {
+	public static void main(String [] args){
+	int a=3;
+	int b=5;
+	int c=10;
+	float d=11;
+	float sum=a+b+c+d;
+	float avg=sum/4;
+	System.out.println("Media = "+avg);
+	}
+}
+
